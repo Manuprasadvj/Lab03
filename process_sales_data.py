83b104e490b4566ff26c0f042cb9cc5e722cb25b
Manuprasadvj <Manuprasad.Variyathjayakumar@flemingcollege.ca>
2023-06-07 19:21:00 -0400

    Update process_sales_data.py

--- process_sales_data.py
+++ process_sales_data.py
@@ -8,6 +8,7 @@ Usage:
 Parameters:
   sales_csv_path = Full path of the sales data CSV file
 """
+import re
 import pandas as pd
 import sys
 import os.path
@@ -71,14 +72,36 @@ def process_sales_data(sales_csv_path, orders_dir_path):
         orders_dir_path (str): Path of orders directory
     """
     # TODO: Import the sales data from the CSV file into a DataFrame
+    df = pd.read_csv(sales_csv_path)
+
+
+
     # TODO: Insert a new "TOTAL PRICE" column into the DataFrame
+    df.insert(7,'TOTAL PRICE', df['ITEM QUANTITY'] * df['ITEM PRICE'])
     # TODO: Remove columns from the DataFrame that are not needed
+    df.drop(columns=['ADDRESS','CITY', 'STATE', 'POSTAL CODE', 'COUNTRY'], inplace=True)
     # TODO: Groups orders by ID and iterate 
+    for order_id, order_df in df.groupby('ORDER ID'):
         # TODO: Remove the 'ORDER ID' column
+        order_df.drop(columns=['ORDER ID'], inplace=True)
         # TODO: Sort the items by item number
+        order_df.sort_values(by='ITEM NUMBER', inplace=True)
         # TODO: Append a "GRAND TOTAL" row
+        grand_total = order_df['TOTAL PRICE'].sum()
+        grand_total_df = pd.DataFrame({'ITEM PRICE': ['GRAND TOTAL'], 'TOTAL PRICE': [grand_total]})
+        order_df = pd.concat([order_df, grand_total_df])
+        
+     
         # TODO: Determine the file name and full path of the Excel sheet
+        customer_name = order_df['CUSTOMER NAME'].values[0]
+        customer_name = re.sub(r'\w', '', customer_name)
+        order_file = f'Order{order_id}_{customer_name}.xlsx'
+        order_excel_path = os.path.join(orders_dir_path, order_file)
         # TODO: Export the data to an Excel sheet
+        worksheet_name = f'Order #{order_id}'
+        order_df.to_excel(order_excel_path, index=False, sheet_name=worksheet_name)
+
+        break
         # TODO: Format the Excel sheet
         # TODO: Define format for the money columns
         # TODO: Format each colunm
